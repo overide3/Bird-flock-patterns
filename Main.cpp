184cff2a625f9aaad770e77bb61ecfdf9cd14ade
Ronan Stevenson <ronanvstevenson.04@gmail.com>
2024-04-09 14:32:41 +0100

    This causes issues with cohesion and seperation

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include "bird.h"
 
 
 int main()
@@ -6,7 +8,16 @@ int main()
 	int WWidth = 1080;
 	int WHeight = 1920;
 	sf::RenderWindow window(sf::VideoMode(WHeight, WWidth), "Window");
-
+	std::vector<Bird> bird_array;
+	int velocity = 1;
+	int vr = 1;
+	int va = 90;
+	int seperation = 5;
+	int no_birds = 100;
+	for (int i = 0; i < no_birds; i++)
+	{
+		bird_array.push_back(Bird(rand() % WHeight, rand() % WWidth, velocity, rand() % 361, vr, va, seperation));
+	}
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -17,7 +28,32 @@ int main()
 				window.close();
 			}
 		}
+		/*
+		for (int i = 0; i < bird_array.size(); i++)
+		{
+			bird_array[i].alignment(bird_array);
+		}
+		*/
+		
+		for (int i = 0; i < bird_array.size(); i++)
+		{
+			bird_array[i].cohesion(bird_array);
+		}
+		/*
+		for (int i = 0; i < bird_array.size(); i++)
+		{
+			bird_array[i].separation(bird_array);
+		}
+		for (int i = 0; i < bird_array.size(); i++)
+		{
+			bird_array[i].update(WWidth,WHeight);
+		}
+		*/
 		window.clear();
+		for (int i = 0; i < bird_array.size(); i++)
+		{
+			window.draw(bird_array[i].get_shape());
+		}
 		window.display();
 	}
 }
